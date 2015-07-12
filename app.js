d885d0a1c3fbf86efa34c311a7d4a0ce6688c472
Bart van der Wal <bartvanderwal@gmail.com>
2015-07-12 19:05:29 +0200

    Added to NPM. Still some TODO's.

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-﻿var fs = require('fs'),
-    // readFile = require('fs-readfile-promise'),
+﻿// Imports.
+var fs = require('fs'),
     xml2js = require('xml2js'),
     _ = require('lodash'),
     Promise = require('promise'),
     glob = require('glob'),
     upath = require('upath'),
-    clone = require('clone'),
+    extend = require('extend'),
     minimatch = require('minimatch');
+
+// Initialize variables.
+var nrOfCheckedFiles = 0,
+    nrOfIncludedFiles = 0,
+    nrOfPhysicalFiles = 0,
+    nrOfTargetedFiles = 0,
+    nrOfNotIncludedFiles = 0,
+    notIncludedFiles = [],
+    maxFilesToShowDefault = 20,
+    options = {};
+
 
 // Polyfills for ES6 functions.
 if (!String.prototype.startsWith) {
@@ -29,13 +40,38 @@ if (!String.prototype.endsWith) {
 }
 
 /**
- *  
- * @param {} csProjFilename 
- * @param {} basePath Optional, the path to find the .csproj file in.
- * @returns {} All files from the given cs project file. If a basepath was given for the .csproj, then this path is added to the path of each of the returned filenames.
+ * Print a debug message to the console (only if debug flag is set).
+ * @param {string} The message to print.
+ */
+function debug(message) {
+    if (options.verbose) {
+        console.log(message);
+    }
+}
+
+/**
+ * Return the path of a given filename.
+ * @param {string} The full path to a file, for instance '../MyApp.Web/App.Web.csproj'.
+ * @returns {string} Only the path to the file excluding the filename, for instance 'App.Web.csproj'
+ */
+function getBasePath(pathAndFile) {
+    if (!pathAndFile) {
+        throw new Error('getBasePath: Supplied path is empty!');
+    }
+    // Remove a potential end slash from path to prevent errors 
+    if (pathAndFile.endsWith('/')) {
+        pathAndFile.substring(0, pathAndFile.length - 1)
+    }
+    var index = pathAndFile.lastIndexOf('/');
+    return index === -1 ? "" : pathAndFile.substring(0, index + 1);
+}
+
+
+/**
+ * @param {string} csProjFilename 
+ * @returns {string} All files from the given cs project file.
  */
 function readCsProjeFile(csProjFilename) {
-    // 5. Lees input file 'Perflectie.Web.csproj' file in.
     var fileIncludes = [];
     var parser = new xml2js.Parser();
     return new Promise(function(resolve, reject) {
@@ -47,7 +83,7 @@ function readCsProjeFile(csProjFilename) {
             parser.parseString(data, function (err, result) {
                 var itemgroups = result.Project.ItemGroup;
                 if (!itemgroups || itemgroups.length === 0) {
-                    reject('Error: No item groups found in ' + csProjFilename);
+                    reject('No item groups found in ' + csProjFilename);
                 }
                 _.each(result.Project.ItemGroup, function(itemGroup, groupKey, groupList) {
                     if ((!itemGroup.Content || itemGroup.Content.length === 0)) {             
@@ -76,76 +112,10 @@ function readCsProjeFile(csProjFilename) {
     });
 }
 
-// 8. Voor elke bestandsnaam B in 'filesToCheck':
-// 9.   Hoog 'nrOfCheckedFiles' met 1 op
-// 10.   Check of de bestandsnaam B in de 'targetedFiles' voorkomt (e.g. is geincluded)
-// 11.	     Zo niet: voeg dan bestandsnaam B toe aan 'errorList' en hoog 'nrOfNotIncludedFiles' met 1 op (zo wel: doe niks :)
-// 12. Tot slot Print op het scherm als er eén of meer filenamen in 'errorList' staan en eventueel verdere variabelen (zie init) op het scherm
-// 13. Geef alle originele input files (e.g. zonder filter) door naar output voor evt. volgende gulp task
 
-// 1. Init:
-var nrOfCheckedFiles = 0,
-    nrOfTargetedFiles = 0,
-    nrOfNotIncludedFiles = 0,
-    notIncludedFiles = [],
-    maxFilesToShowDefault = 20;
-
-// 2. Lees het filter in uit params
-// TODO. For nu even gehardcoded.
-params = {
-    stuffToCheck: ['app/**/*.js', 'Content/**/*.less', 'Content/**/*.css'],
-    csProjFilename: 'Perflectie.Web.csproj',
-    basePath: '../Perflectie.Web',
-    maxFilesToShow: maxFilesToShowDefault,
-    verbose: false
-};
-
-var config = clone(params);
-
-function debug(message) {
-    if (config.verbose) {
-        console.log(message);
-    }
-}
-
-debug('Starting.');
-debug('Params: ');
-debug(params);
-
-if (params.basePath) {
-    // Append a slash if missing.
-    if (!config.basePath.endsWith('/')) {
-        config.basePath += '/';
-    }
-    // Add base path to all patterns in stuff to check.
-    _.each(config.stuffToCheck, function(pattern, patternKey, patternList) {
-        // Remove slash from beginning of globs if present.
-        if (pattern.startsWith('/')) {
-            pattern = pattern.substring(1);
-        }
-        patternList[patternKey] = config.basePath + pattern;
-    });
-    // And prefix the .csproj file with the base path as well.
-    config.csProjFilename = config.basePath + config.csProjFilename;
-}
-//
-
-function getBasePath(pathAndFile) {
-    if (!pathAndFile) {
-        throw new Error('getBasePath: Supplied path is empty!');
-    }
-    // Remove a potential end slash from path to prevent errors 
-    if (pathAndFile.endsWith('/')) {
-        pathAndFile.substring(0, pathAndFile.length-1)
-    }
-    var index = pathAndFile.lastIndexOf('/');
-    return index === -1 ? "" : pathAndFile.substring(0, index+1);
-}
-
-// 3. Lees alle bestandsnamen in uit de genoemde folder in array 'physicalFiles'.
 function searchFilesNotIncludedIn(csProjFilename, filter) {
     return new Promise(function (resolve, reject) {
-        if (config.stuffToCheck.length === 0) {
+        if (filter.length === 0) {
             reject('No stuff to check!');
             return;
         }
@@ -155,12 +125,13 @@ function searchFilesNotIncludedIn(csProjFilename, filter) {
             .then(function(result) {
             return new Promise(function (resolve, reject) {
                 if (!result || result.length === 0) reject('No files in result');
+                nrOfIncludedFiles = result.length;
                 _.each(result, function (filename, fileKey, fileList) {
                     var pattern = filter[0];
                     // Do some conversions to allow simple string comparison to the files as returned by node - glob.
                     // 1. The files in the .csproj are in windows format, so convert to unix;
                     var unixFilename = upath.normalize(filename);
-                    // 2. Add the basePath to the file - if set.
+                    // 2. Add the cwd to the file - if set.
                     if (basePath) {
                         unixFilename = basePath + unixFilename;
                     }
@@ -174,22 +145,18 @@ function searchFilesNotIncludedIn(csProjFilename, filter) {
                     if (fileKey === fileList.length - 1) {
                         resolve(includedFiles);
                     }
-                    // }).then(function() {
-                    // TODO BW Filter non relevant files from includedFiles list based on specified glob patterns (use minimatch library?).
-                    // includedFiles = _.filter.
-                    // });
                 });
-            }).then(function (includedFiles) {
-                _.each(config.stuffToCheck, function (pattern, patternKey, patternList) {
-                    var options = { debug: false };
-                    glob(pattern, options, function (err, files) {
+            }).then(function(includedFiles) {
+                _.each(filter, function(pattern, patternKey, patternList) {
+                    glob(pattern, { debug: options.verbose, cwd: options.cwd }, function(err, files) {
                         if (err) {
                             reject('Error for glob pattern \'' + pattern + '\' in stuff to check!');
                         }
                         if (files.length === 0) {
                             reject('No files for pattern \'' + pattern + '\' in stuff to check.');
                         }
-                        _.each(files, function (filename, fileKey, fileList) {
+                        nrOfTargetedFiles = files.length;
+                        _.each(files, function(filename, fileKey, fileList) {
                             // 4. Filter de bestandsnamen op het filter  bv. alleen **/*.js files - en sla op in array 'filesToCheck'.
                             var isIncluded = _.contains(includedFiles, filename);
                             nrOfCheckedFiles++;
@@ -212,21 +179,70 @@ function searchFilesNotIncludedIn(csProjFilename, filter) {
     });
 }
 
-searchFilesNotIncludedIn(config.csProjFilename, config.stuffToCheck).then(function(notIncludedFiles) {
-    var nrOfNotIncludedFiles = notIncludedFiles.length;
-    if (nrOfNotIncludedFiles > 0) {
-        console.log('There ' + (nrOfNotIncludedFiles==1 ? 'is ' : 'are ') + nrOfNotIncludedFiles + ' of the total ' + nrOfCheckedFiles + ' checked files are NOT included in \'' + config.csProjFilename + '\':');
-        var limitResultsShown = params.maxFilesToShow > 0;
-        var notIncludedFilesShown = limitResultsShown ? notIncludedFiles.slice(0, params.maxFilesToShow) : notIncludedFiles;
-        _.each(notIncludedFilesShown, function (filename) {
-            console.log(filename);
-        });
-        if (limitResultsShown && nrOfNotIncludedFiles > params.maxFilesToShow) {
-            console.log('Only the first ' + params.maxFilesToShow + ' are shown. Change \'maxFilesToShow\' parameter to show more (or less) (default: ' + maxFilesToShowDefault + ')');
-        }
-    } else {
-        console.log('Total ' + nrOfCheckedFiles + ' checked files are included in \'' + config.csProjFilename + '\'.');
+
+function check(patterns, userOptions, csproj) {    
+    var defaultOptions = {
+        csProjFilename: 'Perflectie.Web.csproj',
+        cwd: '../Perflectie.Web',
+        maxFilesToShow: maxFilesToShowDefault,
+        verbose: false
+    };
+    
+    options = extend(defaultOptions, userOptions);
+    
+    // If stuffToCheck contains a single string then convert it to array of one.
+    if (patterns && typeof patterns === 'string') {
+        patterns = [patterns];
     }
-}).catch(function(error) {
-    console.log('Error \'searchFilesNotIncludedIn\': ' + error);
-});
+    
+    options.stuffToCheck = patterns;
+
+    debug('Starting.');
+    debug('Options settings: ');
+    debug(options);
+    
+    if (options.cwd) {
+        // Append a slash if missing.
+        if (!options.cwd.endsWith('/')) {
+            options.cwd += '/';
+        }
+        // Add base path to all patterns in stuff to check.
+        _.each(options.stuffToCheck, function (pattern, patternKey, patternList) {
+            // Remove slash from beginning of globs if present.
+            if (pattern.startsWith('/')) {
+                pattern = pattern.substring(1);
+            }
+            patternList[patternKey] = options.cwd + pattern;
+        });
+        // And prefix the .csproj file with the base path as well.
+        options.csProjFilename = options.cwd + options.csProjFilename;
+    }
+
+    var message;
+    searchFilesNotIncludedIn(options.csProjFilename, options.stuffToCheck).then(function (notIncludedFiles) {
+        var nrOfNotIncludedFiles = notIncludedFiles.length;
+        if (nrOfNotIncludedFiles > 0) {
+            message = 'There ' + (nrOfNotIncludedFiles == 1 ? 'is ' : 'are ') + nrOfNotIncludedFiles + ' of the total ' + nrOfCheckedFiles + ' checked files NOT included in \'' + options.csProjFilename + '\':';
+            console.log(message);
+            var limitResultsShown = options.maxFilesToShow > 0;
+            var notIncludedFilesShown = limitResultsShown ? notIncludedFiles.slice(0, options.maxFilesToShow) : notIncludedFiles;
+            _.each(notIncludedFilesShown, function (filename) {
+                console.log(filename);
+                message += "\n" + filename;
+            });
+            if (limitResultsShown && nrOfNotIncludedFiles > options.maxFilesToShow) {
+                var subMessage = 'Only the first ' + options.maxFilesToShow + ' are shown. Change \'maxFilesToShow\' parameter to show more (or less) (default: ' + maxFilesToShowDefault + ')';
+                console.log(subMessage);
+            }
+        } else {
+            message = 'All ' + nrOfCheckedFiles + ' checked files are included in \'' + options.csProjFilename + '\'.'
+            console.log(message);
+        }
+    }).catch(function(error) {
+        console.log('Error: ' + error);
+    });
+}
+
+exports.check = check;
+
+check(['/Content/**/*.*', '/app/**/*.*'], { cwd: '../Perflectie.Web' }, 'Perflectie.Web.csproj');
---
+++ test.js
@@ -0,0 +1,9 @@
+﻿describe('Check VS Includes', function () {
+    it('should return the nr of not included files when a pattern argument is given that is only a string:)', function() {
+    });
+    it('should work with patterns as an array of strings');
+    it('should return a clear error message work when a .csproj file is specified that does not exist.');
+    it('should return a clear error message work when a .csproj file is specified that does not exist.');
+    it('should return a message in the singular form when only 1 file is not included');
+    it('should return results if one or more itemgroup have not included files, but the last item group does not contain content');
+});
